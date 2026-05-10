02d744241e53e806a6498d8b80e31f0f81ebf280
Pratik Barua <baruaprathik06@gmail.com>
2026-05-10 21:58:05 +0600

    Fixed the toast container issue

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,5 +1,6 @@
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
+import { ToastContainer } from "react-toastify";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -25,6 +26,7 @@ export default function RootLayout({ children }) {
     >
       <body className="min-h-full flex flex-col" data-theme="light">
         {children}
+        <ToastContainer></ToastContainer>
       </body>
     </html>
   );
